c31acb1eda2b04c21b2cebf1bf9496a14f48894f
olegpon <oleg.ponomarev@rasolution.ru>
2019-12-02 11:38:55 +0500

    Initial Commit

---
+++ GetWork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetWork
+//
+//  Created by Max on 02/12/2019.
+//  Copyright © 2019 chulkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
